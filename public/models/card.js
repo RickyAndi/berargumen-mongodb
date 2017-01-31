54bdb259999325fb48b69fcfbc7bbdccd2a37310
ricky <ricky10andika@gmail.com>
2017-01-31 22:30:25 +0700

    Tambah set and get

--- public/models/card.js
+++ public/models/card.js
@@ -1,12 +1,12 @@
-function Card(id, title, content, type, top, left, user, cardId) {
-	this.id = id;
-	this.title = title;
-	this.content = content;
-	this.type = type;
-	this.top = top;
-	this.left = left;
-	this.creator = user;
-	this.relatedTo = cardId;
+function Card() {
+	this.id = null;
+	this.title = null;
+	this.content = null;
+	this.type = null;
+	this.top = null;
+	this.left = null;
+	this.creator = null;
+	this.related = null;
 }
 
 Card.prototype.getId = function() {
@@ -38,7 +38,55 @@ Card.prototype.getCreator = function() {
 }
 
 Card.prototype.getRelatedTo = function() {
-	return this.relatedTo;
+	return this.related.getTo();
+}
+
+Card.prototype.getRelationType = function() {
+	return this.related.getType();
+}
+
+Card.prototype.hasRelation = function() {
+	return this.related.getTo() != null;
+}
+
+Card.prototype.setId = function(id) {
+	this.id = id;
+	return this;
+}
+
+Card.prototype.setTitle = function(title) {
+	this.title = title;
+	return this;
+}
+
+Card.prototype.setContent = function(content) {
+	this.content = content;
+	return this;
+}
+
+Card.prototype.setType = function(type) {
+	this.type = type;
+	return this;
+}
+
+Card.prototype.setTop = function(top) {
+	this.top = top;
+	return this;
+}
+
+Card.prototype.setLeft = function(left) {
+	this.left = left
+	return this;
+}
+
+Card.prototype.setCreator = function(user) {
+	this.creator = user;
+	return this;
+}
+
+Card.prototype.setRelation = function(relation) {
+	this.related = relation;
+	return this;
 }
 
 Card.prototype.isUserCreator = function(user) {
