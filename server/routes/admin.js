11437c936f5febe56299baf8c5031c89e25703b9
ricky <ricky10andika@gmail.com>
2017-02-05 16:34:06 +0700

    Route admin

---
+++ server/routes/admin.js
@@ -0,0 +1,11 @@
+var User = require('../models/user');
+var mongoose = require('mongoose');
+
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+
+module.exports = function(app) {
+	app.get('/admin', async(function(req, res) {
+		
+	}))
+}
