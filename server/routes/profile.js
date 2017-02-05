e706c0e046a640ad923f435b9dae244fdfecaeb7
ricky <ricky10andika@gmail.com>
2017-02-05 16:32:23 +0700

    Tambah route profile

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,6 +1,8 @@
 var User = require('../models/user');
 var mongoose = require('mongoose');
 
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+
 var async = require('asyncawait/async');
 var await = require('asyncawait/await');
 
@@ -12,7 +14,30 @@ module.exports = function(app) {
 		var user = await(User.findById(userId));
 
 		res.render('profile', {
-			user : user
+			profileUser : user,
+			user : req.user
 		});
 	}))
+
+	app.get('/my-profile',  ensureLoggedIn('/login'), async(function(req, res) {
+		
+		try {
+
+			var currentUser = await(User.findById(req.user._id));
+
+			res.render('my-profile', {
+				currentUser : currentUser,
+				title : 'Profil Saya',
+				user : req.user
+			});
+		
+		} catch(error) {
+			
+			res
+				.status(500)
+				.json({
+					message : error.toString()
+				})
+		}
+	}));
 }
