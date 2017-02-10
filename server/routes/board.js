f9d350c8919fd8049be282439a1dd2e77e9a9cad
ricky <ricky10andika@gmail.com>
2017-02-10 13:31:58 +0700

    Route board

---
+++ server/routes/board.js
@@ -0,0 +1,14 @@
+module.exports = function(app) {
+	
+	app.get('/tag/board', function(req, res) {
+		
+		var tag = req.query.tag;
+
+		res.render('board-by-tag', {
+			tag : tag,
+			title : 'Tag : ' + tag
+		});
+
+	});
+
+}
