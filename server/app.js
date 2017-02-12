77b555b7b7d5203ed99285c56992aef1c875407a
ricky <ricky10andika@gmail.com>
2017-02-12 11:16:12 +0700

    Tambah plugin express validator di app

--- server/app.js
+++ server/app.js
@@ -2,13 +2,14 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var express = require('express');
 var passport = require('./passport');
-
+var expressValidator = require('express-validator');
 var app = express();
 
 // set plugins
 app.set('view engine', 'pug');
 app.use(express.static('public'));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(expressValidator());
 app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
