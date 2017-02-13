9bbe9ee85eedc99d99d536dbbc21def7e28eb78a
ricky <ricky10andika@gmail.com>
2017-02-13 17:41:11 +0700

    Rubah callnack url facebook

--- server/passport-config.js
+++ server/passport-config.js
@@ -2,7 +2,7 @@ module.exports = {
 	'facebook' : {
 		clientID: '1785077048484311',
 		clientSecret: '5c1f02db5484b5bca83dcae94c803c3a',
-		callbackURL: 'http://localhost:3000/auth/facebook/callback',
+		callbackURL: 'https://www.berargumen.com/auth/facebook/callback',
 		profileFields: ['id', 'displayName', 'name', 'gender', 'photos', 'email']
 	}
 }
