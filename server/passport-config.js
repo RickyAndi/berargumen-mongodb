f399d3ce9d89132f29cf648e7c79f617d6e8ee6b
ricky <ricky10andika@gmail.com>
2017-02-13 22:00:27 +0700

    Rubah facebook app

--- server/passport-config.js
+++ server/passport-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'facebook' : {
-		clientID: '1785077048484311',
-		clientSecret: '5c1f02db5484b5bca83dcae94c803c3a',
+		clientID: '257524251343117',
+		clientSecret: '6479092115fa8fd4239ba4803e86b15c',
 		callbackURL: 'https://www.berargumen.com/auth/facebook/callback',
 		profileFields: ['id', 'displayName', 'name', 'gender', 'photos', 'email']
 	}
