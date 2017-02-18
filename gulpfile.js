78cdda71c3cd129a40ccfb7b3688f99c9ce2d421
ricky <ricky10andika@gmail.com>
2017-02-18 09:22:59 +0700

    Tambah source maps

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglifyjs');
+var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('build-styles', function() {
 
@@ -69,7 +70,7 @@ gulp.task('copy-fonts', function() {
 	.pipe(gulp.dest('./public/builds/css/font'))
 })
 
-gulp.task('build-script', function() {
+gulp.task('build-script-production', function() {
 
 	// build viewboard.js
 	gulp.src([
@@ -92,9 +93,10 @@ gulp.task('build-script', function() {
 		'./public/vue_components/draggable-card.component.js',
 		'./public/scripts/viewboard.js'
 	])
+	.pipe(sourcemaps.init())
 	.pipe(concat('viewboard.js'))
-	.pipe(gulp.dest('./public/builds/js'))
 	.pipe(uglify())
+	.pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest('./public/builds/js'));
 
 	// build profile.js
@@ -106,9 +108,10 @@ gulp.task('build-script', function() {
 		'./public/models/board.js',
 		'./public/scripts/profile.js'
 	])
+	.pipe(sourcemaps.init())
 	.pipe(concat('profile.js'))
-	.pipe(gulp.dest('./public/builds/js'))
 	.pipe(uglify())
+	.pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest('./public/builds/js'));
 
 	// build myboard.js
@@ -120,9 +123,10 @@ gulp.task('build-script', function() {
 		'./public/scripts/myboard.js'
 
 	])
+	.pipe(sourcemaps.init())
 	.pipe(concat('myboard.js'))
-	.pipe(gulp.dest('./public/builds/js'))
 	.pipe(uglify())
+	.pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest('./public/builds/js'));
 
 	// build my-profile.js
@@ -135,9 +139,10 @@ gulp.task('build-script', function() {
 		'./public/models/board.js',
 		'./public/scripts/my-profile.js'
 	])
+	.pipe(sourcemaps.init())
 	.pipe(concat('my-profile.js'))
-	.pipe(gulp.dest('./public/builds/js'))
 	.pipe(uglify())
+	.pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest('./public/builds/js'));
 
 	// build index.js
@@ -153,9 +158,10 @@ gulp.task('build-script', function() {
 		'./public/models/board.js',
 		'./public/scripts/index.js'
 	])
+	.pipe(sourcemaps.init())
 	.pipe(concat('index.js'))
-	.pipe(gulp.dest('./public/builds/js'))
 	.pipe(uglify())
+	.pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest('./public/builds/js'));
 
 	// build board-by-tag.js
@@ -168,10 +174,125 @@ gulp.task('build-script', function() {
 		'./public/models/user.js',
 		'./public/scripts/board-by-tag.js'
 	])
+	.pipe(sourcemaps.init())
 	.pipe(concat('board-by-tag.js'))
-	.pipe(gulp.dest('./public/builds/js'))
 	.pipe(uglify())
+	.pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest('./public/builds/js'));
 });
 
-gulp.task('default', ['build-script', 'build-styles', 'copy-fonts']);
+gulp.task('build-script-development', function() {
+
+	// build viewboard.js
+	gulp.src([
+		'./public/bower_components/vue/dist/vue.js',
+		'./public/bower_components/jquery/dist/jquery.js',
+		'./public/bower_components/jquery-ui/jquery-ui.js',
+		'./public/bower_components/jqueryui-touch-punch/jquery.ui.touch-punch.js',
+		'./public/bower_components/bootstrap/dist/js/bootstrap.js',
+		'./public/bower_components/summernote/dist/summernote.js',
+		'./public/bower_components/summernote/lang/summernote-id-ID.js',
+		'./public/bower_components/jsplumb/dist/js/jsplumb.js',
+		'./public/bower_components/ramda/dist/ramda.js',
+		'./public/bower_components/socket.io-client/dist/socket.io.js',
+		'./public/bower_components/toastr/toastr.js',
+		'./public/models/board.js',
+		'./public/models/card.js',
+		'./public/models/user.js',
+		'./public/models/relation.js',
+		'./public/forms/create-card.form.js',
+		'./public/vue_components/draggable-card.component.js',
+		'./public/scripts/viewboard.js'
+	])
+	.pipe(sourcemaps.init())
+	.pipe(concat('viewboard.js'))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('./public/builds/js'));
+
+	// build profile.js
+	gulp.src([
+		'./public/bower_components/vue/dist/vue.js',
+		'./public/bower_components/jquery/dist/jquery.js',
+		'./public/bower_components/bootstrap/dist/js/bootstrap.js',
+		'./public/bower_components/moment/min/moment.min.js',
+		'./public/models/board.js',
+		'./public/scripts/profile.js'
+	])
+	.pipe(sourcemaps.init())
+	.pipe(concat('profile.js'))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('./public/builds/js'));
+
+	// build myboard.js
+	gulp.src([
+		'./public/bower_components/vue/dist/vue.js',
+		'./public/bower_components/jquery/dist/jquery.js',
+		'./public/bower_components/bootstrap/dist/js/bootstrap.js',
+		'./public/models/board.js',
+		'./public/scripts/myboard.js'
+
+	])
+	.pipe(sourcemaps.init())
+	.pipe(concat('myboard.js'))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('./public/builds/js'));
+
+	// build my-profile.js
+	gulp.src([
+		'./public/bower_components/vue/dist/vue.js',
+		'./public/bower_components/jquery/dist/jquery.js',
+		'./public/bower_components/bootstrap/dist/js/bootstrap.js',
+		'./public/bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.js',
+		'./public/bower_components/toastr/toastr.js',
+		'./public/models/board.js',
+		'./public/scripts/my-profile.js'
+	])
+	.pipe(sourcemaps.init())
+	.pipe(concat('my-profile.js'))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('./public/builds/js'));
+
+	// build index.js
+	gulp.src([
+		'./public/bower_components/jquery/dist/jquery.js',
+		'./public/bower_components/bootstrap/dist/js/bootstrap.js',
+		'./public/bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.js',
+		'./public/bower_components/vue/dist/vue.js',
+		'./public/bower_components/moment/moment.js',
+		'./public/bower_components/rxjs/dist/rx.lite.js',
+		'./public/bower_components/toastr/toastr.js',
+		'./public/models/user.js',
+		'./public/models/board.js',
+		'./public/scripts/index.js'
+	])
+	.pipe(sourcemaps.init())
+	.pipe(concat('index.js'))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('./public/builds/js'));
+
+	// build board-by-tag.js
+	gulp.src([
+		'./public/bower_components/jquery/dist/jquery.js',
+		'./public/bower_components/bootstrap/dist/js/bootstrap.js',
+		'./public/bower_components/vue/dist/vue.js',
+		'./public/bower_components/moment/moment.js',
+		'./public/models/board.js',
+		'./public/models/user.js',
+		'./public/scripts/board-by-tag.js'
+	])
+	.pipe(sourcemaps.init())
+	.pipe(concat('board-by-tag.js'))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('./public/builds/js'));
+});
+
+gulp.task('watch-development', function() {
+	return gulp.watch([
+		'./public/scripts/*.js',
+		'./public/vue_components/*.js'
+	], ['build-development'])
+})
+
+gulp.task('default', ['build-script-production', 'build-styles', 'copy-fonts']);
+gulp.task('build-development', ['build-script-development', 'build-styles', 'copy-fonts']);
+gulp.task('build-production', ['build-script-production', 'build-styles', 'copy-fonts']);
